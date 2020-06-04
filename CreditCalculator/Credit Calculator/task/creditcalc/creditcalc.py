2c3711ec71a07d5497d4a2499a6375aa50e93507
akocur <akocur@yandex.ru>
2020-06-04 20:53:06 +0300

    Credit calculator. 'Stage #1:Beginning' is solved

--- CreditCalculator/Credit Calculator/task/creditcalc/creditcalc.py
+++ CreditCalculator/Credit Calculator/task/creditcalc/creditcalc.py
@@ -4,4 +4,6 @@ first_month = 'Month 1: paid out 250'
 second_month = 'Month 2: paid out 250'
 third_month = 'Month 3: paid out 500'
 
-# write your code here
+print(credit_principal, first_month, second_month, third_month, final_output, sep='\n')
+
+
