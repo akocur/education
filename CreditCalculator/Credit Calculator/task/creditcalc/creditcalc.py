8a41137a3b1bba8973425f6042bd1e48f84e111d
akocur <akocur@yandex.ru>
2020-06-06 01:38:24 +0300

    Credit Calculator. Stage 3/4: Annuity payment. Complete.

--- CreditCalculator/Credit Calculator/task/creditcalc/creditcalc.py
+++ CreditCalculator/Credit Calculator/task/creditcalc/creditcalc.py
@@ -1,25 +1,51 @@
-print('Enter the credit principal')
-credit_principal = int(input('> '))
+from math import log, ceil, floor
+
+
+def input_remaining_values():
+    global answer, credit_principal, monthly_payment, n_month, credit_interest
+
+    if answer not in ['n', 'a', 'p']:
+        print('Error. Unknown command.')
+        return
+    if not answer == 'p':
+        print('Enter credit principal:')
+        credit_principal = int(input('> '))
+    if not answer == 'a':
+        print('Enter monthly payment:')
+        monthly_payment = float(input())
+    if not answer == 'n':
+        print('Enter count of periods:')
+        n_month = int(input())
+    print('Enter credit interest:')
+    credit_interest = float(input())
+
+
+def output_result():
+    global answer, credit_principal, monthly_payment, n_month, credit_interest
+
+    i = credit_interest / 1200
+    if answer == 'p':
+        credit_principal = floor(monthly_payment * (pow(1 + i, n_month) - 1) / (i * pow(1 + i, n_month)))
+        print(f'Your credit principal = {credit_principal}!')
+    elif answer == 'a':
+        monthly_payment = ceil(credit_principal * i * pow(1 + i, n_month) / (pow(1 + i, n_month) - 1))
+        print(f'Your annuity payment = {monthly_payment}!')
+    elif answer == 'n':
+        n_month = ceil(log(monthly_payment / (monthly_payment - i * credit_principal), 1 + i))
+        n_years = n_month // 12
+        n_month %= 12
+        n_years_str = '' if n_years < 1 else f'{n_years} year{"s" if n_years > 1 else ""}'
+        and_str = ' and ' if n_years > 0 and n_month > 0 else ''
+        n_month_str = '' if n_month < 1 else f'{n_month} month{"s" if n_month > 1 else ""}'
+        print(f'You need {n_years_str}{and_str}{n_month_str} to repay this credit!')
+
+
+credit_principal = monthly_payment = n_month = credit_interest = 0
 print('What do you want to calculate?',
-      'type "m" - for count of months',
+      'type "n" - for count of months',
+      'type "a" - for annuity monthly payment',
       'type "p" - for monthly payment',
       sep='\n')
-answer = input()
-if answer == 'm':
-    print('Enter monthly payment:')
-    monthly_payment = int(input())
-    n_month = credit_principal // monthly_payment
-    if credit_principal % monthly_payment:
-        n_month += 1
-    print('\nIt take {} month{} to repay the credit'.format(n_month, 's' if n_month > 1 else ''))
-elif answer == 'p':
-    print('Enter count of months:')
-    n_month = int(input())
-    monthly_payment = credit_principal // n_month
-    if credit_principal % n_month:
-        monthly_payment += 1
-    last_month_payment = credit_principal - (n_month - 1) * monthly_payment
-    addition = '' if last_month_payment == monthly_payment else f' with last month payment = {last_month_payment}'
-    print(f'Your monthly payment = {monthly_payment}{addition}')
-else:
-    print('Error. Unknown command.')
+answer = input('> ')
+input_remaining_values()
+output_result()
