d4c8b12c3e70d6abc391385806de5aadfba07068
akocur <akocur@yandex.ru>
2020-06-14 23:07:35 +0300

    Credit Calculator. Stage 4/4: Differentiate payment. Complete.

--- CreditCalculator/Credit Calculator/task/creditcalc/creditcalc.py
+++ CreditCalculator/Credit Calculator/task/creditcalc/creditcalc.py
@@ -1,51 +1,85 @@
 from math import log, ceil, floor
+import argparse
 
 
-def input_remaining_values():
-    global answer, credit_principal, monthly_payment, n_month, credit_interest
+class CreditCalculator:
+    def __init__(self, type_calc='annuity', payment=0, periods=0, interest=0, principal=0):
+        self.type = type_calc
+        self.payment = payment
+        self.periods = periods
+        self.interest = interest
+        self.principal = principal
+        self.interest_rate = self.interest / 1200
+        self.total_payments = 0
 
-    if answer not in ['n', 'a', 'p']:
-        print('Error. Unknown command.')
-        return
-    if not answer == 'p':
-        print('Enter credit principal:')
-        credit_principal = int(input('> '))
-    if not answer == 'a':
-        print('Enter monthly payment:')
-        monthly_payment = float(input())
-    if not answer == 'n':
-        print('Enter count of periods:')
-        n_month = int(input())
-    print('Enter credit interest:')
-    credit_interest = float(input())
+    def __repr__(self):
+        return f'''
+            type: {self.type}
+            payment: {self.payment}
+            periods: {self.periods}
+            interest: {self.interest}
+            principal: {self.principal}
+            interest_rate: {self.interest_rate}
+            total_payments: {self.total_payments}
+            '''
+
+    def overpayment(self):
+        print(f'\nOverpayment = {self.total_payments - self.principal}')
+
+    def are_errors(self):
+        if self.type not in ['annuity', 'diff']:
+            return True
+        if self.interest <= 0 or self.payment < 0 or self.periods < 0 or self.principal < 0:
+            return True
+        if self.type == 'annuity':
+            if self.payment == self.periods == self.principal == 0:
+                return True
+        else:
+            if self.payment:
+                return True
+
+    def calculate(self):
+        if self.are_errors():
+            print('Incorrect parameters')
+            return
+
+        if self.type == 'annuity':
+            if self.principal == 0:
+                self.principal = floor(self.payment * (pow(1 + self.interest_rate, self.periods) - 1) /
+                                       (self.interest_rate * pow(1 + self.interest_rate, self.periods)))
+                print(f'Your credit principal = {self.principal}!')
+            elif self.payment == 0:
+                self.payment = ceil(self.principal * self.interest_rate * pow(1 + self.interest_rate, self.periods) /
+                                    (pow(1 + self.interest_rate, self.periods) - 1))
+                print(f'Your annuity payment = {self.payment}!')
+            elif self.periods == 0:
+                self.periods = ceil(log(self.payment / (self.payment - self.interest_rate * self.principal),
+                                        1 + self.interest_rate))
+                n_years = self.periods // 12
+                n_month = self.periods % 12
+                n_years_str = '' if n_years < 1 else f'{n_years} year{"s" if n_years > 1 else ""}'
+                and_str = ' and ' if n_years > 0 and n_month > 0 else ''
+                n_month_str = '' if n_month < 1 else f'{n_month} month{"s" if n_month > 1 else ""}'
+                print(f'You need {n_years_str}{and_str}{n_month_str} to repay this credit!')
+            self.total_payments = self.payment * self.periods
+        elif self.type == 'diff':
+            sum_payment = 0
+            for month in range(1, self.periods + 1):
+                payment = ceil(self.principal / self.periods + self.interest_rate * (self.principal - self.principal *
+                                                                                     (month - 1) / self.periods))
+                print(f'Month {month}: paid out {payment}')
+                sum_payment += payment
+            self.total_payments = sum_payment
+        self.overpayment()
 
 
-def output_result():
-    global answer, credit_principal, monthly_payment, n_month, credit_interest
+parser_args = argparse.ArgumentParser()
+parser_args.add_argument('--type')
+parser_args.add_argument('--payment', type=int, default=0)
+parser_args.add_argument('--principal', type=int, default=0)
+parser_args.add_argument('--periods', type=int, default=0)
+parser_args.add_argument('--interest', type=float, default=0)
+args = parser_args.parse_args()
 
-    i = credit_interest / 1200
-    if answer == 'p':
-        credit_principal = floor(monthly_payment * (pow(1 + i, n_month) - 1) / (i * pow(1 + i, n_month)))
-        print(f'Your credit principal = {credit_principal}!')
-    elif answer == 'a':
-        monthly_payment = ceil(credit_principal * i * pow(1 + i, n_month) / (pow(1 + i, n_month) - 1))
-        print(f'Your annuity payment = {monthly_payment}!')
-    elif answer == 'n':
-        n_month = ceil(log(monthly_payment / (monthly_payment - i * credit_principal), 1 + i))
-        n_years = n_month // 12
-        n_month %= 12
-        n_years_str = '' if n_years < 1 else f'{n_years} year{"s" if n_years > 1 else ""}'
-        and_str = ' and ' if n_years > 0 and n_month > 0 else ''
-        n_month_str = '' if n_month < 1 else f'{n_month} month{"s" if n_month > 1 else ""}'
-        print(f'You need {n_years_str}{and_str}{n_month_str} to repay this credit!')
-
-
-credit_principal = monthly_payment = n_month = credit_interest = 0
-print('What do you want to calculate?',
-      'type "n" - for count of months',
-      'type "a" - for annuity monthly payment',
-      'type "p" - for monthly payment',
-      sep='\n')
-answer = input('> ')
-input_remaining_values()
-output_result()
+credit_calc = CreditCalculator(args.type, args.payment, args.periods, args.interest, args.principal)
+credit_calc.calculate()
