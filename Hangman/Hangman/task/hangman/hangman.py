cdafed77768753de67fbec39e91306a838fc281b
akocur <akocur@yandex.ru>
2020-06-19 13:48:18 +0300

    Project: Hangman. Stage 4/8: Help is on the way. Complete.

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -4,5 +4,6 @@ import random
 print('H A N G M A N')
 words = ('python', 'java', 'kotlin', 'javascript')
 word = random.choice(words)
-answer = input('Guess the word: > ')
+hint = word[:3] + '-' * (len(word) - 3)
+answer = input(f'Guess the word: {hint}  > ')
 print('You survived!' if word == answer else 'You are hanged!')
