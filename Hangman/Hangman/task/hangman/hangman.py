e600d67aa60dea94428ec8317a9a3a68fd17cb2f
akocur <akocur@yandex.ru>
2020-06-19 23:04:37 +0300

    Project: Hangman. Stage 5/8: Keep trying. Complete.

--- Hangman/Hangman/task/hangman/hangman.py
+++ Hangman/Hangman/task/hangman/hangman.py
@@ -1,9 +1,30 @@
 import random
 
 
+def shows_result():
+    global word, input_correct_letters
+
+    result = '\n'
+    for letter in word:
+        if letter in input_correct_letters:
+            result += letter
+        else:
+            result += '-'
+    print(result)
+
+
 print('H A N G M A N')
 words = ('python', 'java', 'kotlin', 'javascript')
 word = random.choice(words)
-hint = word[:3] + '-' * (len(word) - 3)
-answer = input(f'Guess the word: {hint}  > ')
-print('You survived!' if word == answer else 'You are hanged!')
+letters_of_word = set(word)
+input_correct_letters = set()
+
+for _i in range(8):
+    shows_result()
+    input_letter = input('Input a letter: > ')
+    if input_letter in letters_of_word:
+        input_correct_letters.add(input_letter)
+    else:
+        print('No such letter in the word')
+
+print('\nThanks for playing!', "We'll see how well you did in the next stage", sep='\n')
