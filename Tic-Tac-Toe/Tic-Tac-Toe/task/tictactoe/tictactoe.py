2e9be40d28f59a64128a3e388d9ad11ee1f62d96
akocur <akocur@yandex.ru>
2020-06-20 17:21:33 +0300

    Project: Tic-Tac-Toe. Stage 2/5: The user is the gamemaster. Complete.

--- Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,1 +1,5 @@
-print('X O X', 'O X O', 'X X O', sep='\n')
+cells = input('Enter cells: > ')
+print('---------')
+for i in range(3):
+    print('|', cells[i * 3 + 0], cells[i * 3 + 1], cells[i * 3 + 2], '|')
+print('---------')
