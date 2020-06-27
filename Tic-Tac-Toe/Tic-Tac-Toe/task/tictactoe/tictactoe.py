453b00a7a621fe5d867c7ff21a369ac87ee6552f
akocur <akocur@yandex.ru>
2020-06-27 12:44:07 +0300

    Project: Tic-Tac-Toe. Stage 4/5: First move! Complete.

--- Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,7 +1,7 @@
 class TicTacToe:
 
-    def __init__(self):
-        self.cells = [['_' for _ in range(3)] for __ in range(3)]
+    def __init__(self, data='_'):
+        self.cells = [[data[i * 3 + j] for j in range(3)] for i in range(3)]
         self.state = 'Game not finished'
         self.players = {'X': [False, 0],  # [is_win, count]
                         'O': [False, 0]}
@@ -14,7 +14,6 @@ class TicTacToe:
                 print(self.cells[i][j], end=' ')
             print('|')
         print('---------')
-        print(self.state)
 
     def checks_is_player_winner(self, player):
         for row in self.cells:
@@ -56,15 +55,39 @@ class TicTacToe:
         elif self.players['X'][1] + self.players['O'][1] == 9:
             self.state = 'Draw'
 
-    def input(self):
-        input_data = input('Enter cells: > ')
-        self.cells = [[input_data[i * 3 + j] for j in range(3)] for i in range(3)]
+    def is_correct_coordinates(self, coordinates):
+        if not len(coordinates) == 2:
+            print('There should be two coordinates!')
+            return False
+        for coordinate in coordinates:
+            if not coordinate.isdigit():
+                print('You should enter numbers!')
+                return False
+            if int(coordinate) not in range(1, 4):
+                print('Coordinates should be from 1 to 3!')
+                return False
+        return True
+
+    def enter_coordinates(self):
+        while True:
+            coordinates = input('Enter the coordinates: > ').split()
+            if not self.is_correct_coordinates(coordinates):
+                continue
+            column, row = int(coordinates[0]) - 1, 3 - int(coordinates[1])
+            if not self.cells[row][column] == '_':
+                print('This cell is occupied! Choose another one!')
+                continue
+            self.cells[row][column] = 'X'
+            break
 
     def play(self):
-        self.input()
+        self.sets_state()
+        self.display()
+        self.enter_coordinates()
         self.sets_state()
         self.display()
 
 
-game = TicTacToe()
+input_data = input('Enter cells: > ')
+game = TicTacToe(input_data)
 game.play()
