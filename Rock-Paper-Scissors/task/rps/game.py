fa01159a01f3a030173a367a3d8b95be6371f1a1
akocur <akocur@yandex.ru>
2020-06-30 20:59:18 +0300

    Project: Rock-Paper-Scissors. Stage 2/5: Equalizing chances. Complete.

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,4 +1,32 @@
-accordance = {'rock': 'paper',
-              'paper': 'scissors',
-              'scissors': 'rock'}
-print(f'Sorry, but computer chose {accordance[input()]}')
+import random
+
+
+class RockPaperScissors:
+    lose_options = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+
+    def __init__(self, user_choice):
+        self.computer_choice = random.choice(['rock', 'paper', 'scissors'])
+        self.user_choice = user_choice
+        self.status = None
+
+    def set_status(self):
+        if RockPaperScissors.lose_options[self.user_choice] == self.computer_choice:
+            self.status = 'lose'
+        elif self.computer_choice == self.user_choice:
+            self.status = 'draw'
+        else:
+            self.status = 'win'
+
+    def prints_result(self):
+        statuses = {'lose': f'Sorry, but computer chose {self.computer_choice}',
+                    'draw': f'There is a draw ({self.computer_choice})',
+                    'win': f'Well done. Computer chose {self.computer_choice} and failed'}
+        print(statuses[self.status])
+
+    def play(self):
+        self.set_status()
+        self.prints_result()
+
+
+game = RockPaperScissors(input())
+game.play()
