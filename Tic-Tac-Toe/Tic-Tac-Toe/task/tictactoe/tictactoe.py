3d1c28c0f892eb4a6837a88dc37f757e35d28826
akocur <akocur@yandex.ru>
2020-06-30 16:33:33 +0300

    Project: Tic-Tac-Toe. Stage 5/5: Fight! Complete.

--- Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,10 +1,11 @@
 class TicTacToe:
 
-    def __init__(self, data='_'):
+    def __init__(self, data='_' * 9):
         self.cells = [[data[i * 3 + j] for j in range(3)] for i in range(3)]
         self.state = 'Game not finished'
         self.players = {'X': [False, 0],  # [is_win, count]
                         'O': [False, 0]}
+        self.current_player = 'X'
 
     def display(self):
         print('---------')
@@ -14,37 +15,27 @@ class TicTacToe:
                 print(self.cells[i][j], end=' ')
             print('|')
         print('---------')
+        print(self.state)
 
     def checks_is_player_winner(self, player):
         for row in self.cells:
-            number_in_row = sum([1 if cell == player else 0 for cell in row])
-            if number_in_row == 3:
+            if all(1 if cell == player else 0 for cell in row):
                 self.players[player][0] = True
                 return
-
         columns = [[self.cells[r][c] for r in range(3)] for c in range(3)]
         for column in columns:
-            number_in_column = sum([1 if cell == player else 0 for cell in column])
-            if number_in_column == 3:
+            if all(1 if cell == player else 0 for cell in column):
                 self.players[player][0] = True
                 return
-
         diagonals = [[self.cells[i][i] for i in range(3)], [self.cells[i][2 - i] for i in range(3)]]
         for diagonal in diagonals:
-            number_in_diagonal = sum(1 if cell == player else 0 for cell in diagonal)
-            if number_in_diagonal == 3:
+            if all(1 if cell == player else 0 for cell in diagonal):
                 self.players[player][0] = True
                 return
 
     def sets_state(self):
-        self.state = 'Game not finished'
         self.checks_is_player_winner('X')
         self.checks_is_player_winner('O')
-        for row in self.cells:
-            for cell in row:
-                if cell == '_':
-                    continue
-                self.players[cell][1] += 1  # incriment count
         if self.players['X'][0] == self.players['O'][0] == True \
                 or abs(self.players['X'][1] - self.players['O'][1]) > 1:
             self.state = 'Impossible'
@@ -55,7 +46,8 @@ class TicTacToe:
         elif self.players['X'][1] + self.players['O'][1] == 9:
             self.state = 'Draw'
 
-    def is_correct_coordinates(self, coordinates):
+    @staticmethod
+    def is_correct_coordinates(coordinates):
         if not len(coordinates) == 2:
             print('There should be two coordinates!')
             return False
@@ -68,6 +60,12 @@ class TicTacToe:
                 return False
         return True
 
+    def set_current_player(self):
+        if self.current_player == 'X':
+            self.current_player = 'O'
+        else:
+            self.current_player = 'X'
+
     def enter_coordinates(self):
         while True:
             coordinates = input('Enter the coordinates: > ').split()
@@ -77,17 +75,19 @@ class TicTacToe:
             if not self.cells[row][column] == '_':
                 print('This cell is occupied! Choose another one!')
                 continue
-            self.cells[row][column] = 'X'
+            self.cells[row][column] = self.current_player
+            self.players[self.current_player][1] += 1  # incriment count
+            self.set_current_player()
             break
 
     def play(self):
-        self.sets_state()
         self.display()
-        self.enter_coordinates()
-        self.sets_state()
-        self.display()
+        while self.state not in ['X wins', 'O wins', 'Draw']:
+            self.enter_coordinates()
+            self.sets_state()
+            self.display()
+        print(self.state)
 
 
-input_data = input('Enter cells: > ')
-game = TicTacToe(input_data)
+game = TicTacToe()
 game.play()
