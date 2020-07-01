4e3b7cc610a31bbc326a66361397227614c84482
akocur <akocur@yandex.ru>
2020-07-01 21:40:36 +0300

    Project: Rock-Paper-Scissors. Problem 'Missing'. Complete.

--- Rock-Paper-Scissors/task/tests.py
+++ Rock-Paper-Scissors/task/tests.py
@@ -12,26 +12,55 @@ class RPSTest(StageTest):
         self.wins = 0
         self.draws = 0
         self.loses = 0
+        self.file_name = 'rating.txt'
 
     def generate(self) -> List[TestCase]:
-        valid_input_cases = ["rock\npaper\nscissors\npaper\nscissors\nrock\npaper\nscissors\n!exit",
-                             "scissors\nscissors\nscissors\n!exit"]
-        invalid_input_cases = ["rock\npaper\npaper\nscissors\nblabla\n!exit",
-                               "rock\ninvalid\n!exit",
-                               "rock\nrock\nrock\nrock-n-roll\n!exit"]
+        valid_input_cases = ["Tom\nrock\npaper\nscissors\npaper\nscissors\nrock\npaper\nscissors\n!exit",
+                             "Tom\nscissors\nscissors\nscissors\n!exit"]
+        invalid_input_cases = ["Tom\nrock\npaper\npaper\nscissors\nblabla\n!exit",
+                               "Tom\nrock\ninvalid\n!exit",
+                               "Tom\nrock\nrock\nrock\nrock-n-roll\n!exit"]
         tests = list()
-        # Cases that checks multiple input
-        [tests.append(TestCase(stdin=inp, attach=len(inp.split('\n')) - 1, check_function=self.check_valid_inputs)) for
-         inp in valid_input_cases]
+        # Cases that check multiple input
+        [tests.append(
+            TestCase(
+                stdin=inp,
+                attach=len(inp.split('\n')) - 2,
+                check_function=self.check_valid_inputs,
+                files={self.file_name: 'Bob 350\nJane 200\nAlex 400'}
+            )
+        ) for inp in valid_input_cases]
         # Cases that check invalid input
-        [tests.append(TestCase(stdin=inp, check_function=self.check_invalid_input)) for inp in invalid_input_cases]
-        # Cases that check using random module
-        long_input = ''
+        [tests.append(
+            TestCase(
+                stdin=inp,
+                check_function=self.check_invalid_input,
+                files={self.file_name: 'Bob 350\nJane 200\nAlex 400'}
+            )
+        ) for inp in invalid_input_cases]
+        # Case that checks using random module
+        long_input = 'Tim\n'
         for _ in range(100):
             long_input += 'rock\n'
         long_input += '!exit'
-        tests.append(TestCase(stdin=long_input, attach='rock', check_function=self.check_results))
-
+        tests.append(
+            TestCase(
+                stdin=long_input,
+                attach='rock',
+                check_function=self.check_results,
+                files={self.file_name: 'Bob 350\nJane 200\nAlex 400'}
+            ))
+        # Case that checks pints in file
+        temp = long_input.split('\n')
+        temp[-2] = '!rating'
+        long_input = '\n'.join(temp)
+        tests.append(
+            TestCase(
+                stdin=long_input,
+                attach='rock',
+                check_function=self.check_file,
+                files={self.file_name: 'Bob 350\nJane 200\nAlex 400'}
+            ))
         return tests
 
     @staticmethod
@@ -55,7 +84,28 @@ class RPSTest(StageTest):
                                      .format(attach, results))
         return CheckResult.correct()
 
-    def check_results(self, reply: str, attach) -> CheckResult:
+    def check_results(self, reply: str, ignored) -> CheckResult:
+
+        self.wins = reply.lower().count('well done')
+        self.draws = reply.lower().count('draw')
+        self.loses = reply.lower().count('sorry')
+
+        wrong_randomize = CheckResult.wrong("The results of the games: {} wins, {} draws and {} loses\n"
+                                            "Looks like you don't use random module to choose random option!\n"
+                                            "The number of wins, draws and loses should be approximately the same.\n"
+                                            "Make sure you output the results of the games like in examples!\n"
+                                            "If you pretty sure that you use random module try to rerun the tests!\n"
+                                            .format(self.wins, self.draws, self.loses))
+        if self.loses < 20:
+            return wrong_randomize
+        if self.draws < 20:
+            return wrong_randomize
+        if self.wins < 20:
+            return wrong_randomize
+
+        return CheckResult.correct()
+
+    def check_file(self, reply: str, ignored) -> CheckResult:
 
         for line in reply.split('\n'):
             lower_line = line.lower()
@@ -74,18 +124,12 @@ class RPSTest(StageTest):
         self.draws = reply.lower().count('draw')
         self.loses = reply.lower().count('sorry')
 
-        wrong_randomize = CheckResult.wrong("The results of the games: {} wins, {} draws and {} loses\n"
-                                            "Looks like you don't use random module to choose random option!\n"
-                                            "The number of wins, draws and loses should be approximately the same.\n"
-                                            "Make sure you output the results of the games like in examples!\n"
-                                            "If you pretty sure that you use random module try to rerun the tests!\n"
-                                            .format(self.wins, self.draws, self.loses))
-        if self.loses < 20:
-            return wrong_randomize
-        if self.draws < 20:
-            return wrong_randomize
-        if self.wins < 20:
-            return wrong_randomize
+        correct_points = self.wins * 100 + self.draws * 50
+
+        if str(correct_points) not in reply:
+            return CheckResult.wrong('Looks like you incorrectly calculated the player\'s score!\n'
+                                     'For each draw, add 50 point to the score. For each user\'s win, add 100 to his/her score.\n'
+                                     'In case the user loses, don\'t change the score. ')
 
         return CheckResult.correct()
 
