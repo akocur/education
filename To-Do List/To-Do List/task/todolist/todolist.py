c6de827f21f01b0cf0d040113963aa9d202f2cc8
akocur <akocur@yandex.ru>
2020-07-28 19:57:22 +0300

    Project:  To-Do List. Stage 1/4: Plan it. Complete.

--- To-Do List/To-Do List/task/todolist/todolist.py
+++ To-Do List/To-Do List/task/todolist/todolist.py
@@ -1,1 +1,11 @@
-# Write your code here
+def show_tasks(date, tasks):
+    print(f'{date}:')
+    for i in range(len(tasks)):
+        print(f'{i + 1}) {tasks[i]}')
+
+
+tasks = ['Do yoga',
+         'Make breakfast',
+         'Learn basics of SQL',
+         'Learn what is ORM']
+show_tasks('Today', tasks)
