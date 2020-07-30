45c31831c47eae2162723bff1e28b27052ecb949
akocur <akocur@yandex.ru>
2020-07-30 18:44:08 +0300

    Project:  To-Do List. Problem 'Bittersweet occasion'. Complete.

---
+++ To-Do List/Problems/Bittersweet occasion/task.py
@@ -0,0 +1,6 @@
+# finish the function
+def find_the_parent(child):
+    for parent_class in Drinks, Pastry, Sweets:
+        if issubclass(child, parent_class):
+            print(parent_class.__name__)
+            break
