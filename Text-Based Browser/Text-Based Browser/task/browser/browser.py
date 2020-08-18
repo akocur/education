af811f46d3cb99efecd06e21488799479f6c6451
akocur <akocur@yandex.ru>
2020-08-18 20:39:18 +0300

    Project: Text-Based Browser. Stage 4/6: Requesting. Complete.

--- Text-Based Browser/Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/Text-Based Browser/task/browser/browser.py
@@ -1,44 +1,11 @@
 import argparse
 import os
-
-
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
+from collections import deque
+import requests
 
 
 def get_short_url(url):
+    url = url.replace('https://', '')
     dot_position = url.rfind('.')
     return url if dot_position == -1 else url[:dot_position]
 
@@ -48,7 +15,11 @@ def is_url_correct(url, folder):
 
 
 def is_url_exist(url):
-    return url in ('bloomberg.com', 'nytimes.com')
+    if url.rfind('.') == -1:
+        return False
+    if not url.startswith('https://'):
+        url = 'https://' + url
+    return True if requests.get(url) else False
 
 
 def print_tab(url, folder):
@@ -59,9 +30,11 @@ def print_tab(url, folder):
 def save_tab(url, folder):
     if not is_url_exist(url):
         return
-    tab = globals()[url.replace('.', '_')]
+    if not url.startswith('https://'):
+        url = 'https://' + url
+    tab = requests.get(url)
     with open(folder + get_short_url(url), 'w') as file:
-        file.write(tab)
+        file.write(tab.text)
 
 
 args_parser = argparse.ArgumentParser()
@@ -72,11 +45,20 @@ folder = args.folder + '/'
 if not os.path.exists(folder):
     os.makedirs(folder)
 
+web_history = deque()
 answer = input()
 while not answer == 'exit':
-    if is_url_correct(answer, folder):
+    if answer == 'back':
+        if len(web_history) > 1:
+            web_history.pop()  # current page
+            # This is code block because deque don't have peek() method
+            back_page = web_history.pop()
+            print_tab(back_page, folder)
+            web_history.append(back_page)
+    elif is_url_correct(answer, folder):
         save_tab(answer, folder)
         print_tab(answer, folder)
+        web_history.append(answer)
     else:
         print('Error: Incorrect URL\n')
     answer = input()
