f67a8dce3e14cb186ac6c405f7c3bb7a8bde1edb
akocur <akocur@yandex.ru>
2020-08-28 21:35:56 +0300

    Project: Smart Calculator. Stage 5/7: Error! Complete.

--- Smart Calculator/Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -1,36 +1,56 @@
-from collections import deque
+def main():
+    smart_calculator = SmartCalculator()
+    answer = input().strip()
+    while answer != '/exit':
+        smart_calculator.handle(answer)
+        answer = input().strip()
+    print('Bye!')
 
 
 class SmartCalculator:
+    available_operations = ('-', '+')
+    available_commands = ('/help',)
+
     def __init__(self):
         self.result = 0
-        self.input = None
+        self.input = []
+
+    def add(self, number):
+        self.result += number
 
     def calculate(self):
         self.result = 0
-        sign = 1
         for x in self.input:
-            if SmartCalculator.is_number(x):
-                self.add(sign * int(x))
-            else:
-                sign = -1 if x.count('-') % 2 else 1
-
-    @staticmethod
-    def is_number(number):
-        try:
-            float(number)
-            return True
-        except ValueError:
-            return False
+            self.add(x)
 
     def handle(self, input_str):
         if input_str == '/help':
             self.print_help()
         else:
-            if input_str:
-                self.input = input_str.strip().split()
+            if self.parse_input(input_str):
                 self.calculate()
                 print(self.result)
+
+    def parse_input(self, input_str):
+        if input_str:
+            self.input.clear()
+            sign = 1
+            for x in input_str.strip().split():
+                try:
+                    self.input.append(sign * int(x))
+                except ValueError:
+                    if x[0] == '/' and len(x) > 1:
+                        print('Unknown command')
+                        return False
+                    if not all(x[0] == x[i] for i in range(len(x))):
+                        print('Invalid expression')
+                        return False
+                    if x[0] not in SmartCalculator.available_operations:
+                        print('Invalid expression')
+                        return False
+                    sign = -1 if x.count('-') % 2 else 1
+            return True
+        return False
 
     @staticmethod
     def print_help():
@@ -40,18 +60,6 @@ class SmartCalculator:
               '9 +++ 10 -- 8 equals 27\n'
               '3 --- 5 equals -2\n'
               '14       -   12 equals 2\n')
-
-    def add(self, number):
-        self.result += number
-
-
-def main():
-    smart_calculator = SmartCalculator()
-    answer = input().strip()
-    while answer != '/exit':
-        smart_calculator.handle(answer)
-        answer = input().strip()
-    print('Bye!')
 
 
 if __name__ == '__main__':
