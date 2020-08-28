bc918cfe662fc81389b9408f3ea97f60a036e733
akocur <akocur@yandex.ru>
2020-08-28 17:16:41 +0300

    Project: Smart Calculator. Stage 3/7: Count them all. Complete.

--- Smart Calculator/Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -1,8 +1,28 @@
+class SmartCalculator:
+    def __init__(self):
+        self.input = None
+
+    def sum(self):
+        return sum(int(x) for x in self.input.split())
+
+    @staticmethod
+    def print_help():
+        print('The program calculates the sum of numbers')
+
+    def handle(self, input_str):
+        self.input = input_str.strip()
+        if self.input == '/help':
+            self.print_help()
+        else:
+            if self.input:
+                print(self.sum())
+
+
 def main():
+    smart_calculator = SmartCalculator()
     answer = input().strip()
     while answer != '/exit':
-        if answer:
-            print(sum(int(x) for x in answer.split()))
+        smart_calculator.handle(answer)
         answer = input().strip()
     print('Bye!')
 
