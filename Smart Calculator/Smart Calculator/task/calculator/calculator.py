35405a180d17efeff3ff8a3d158545f49a2b3e75
akocur <akocur@yandex.ru>
2020-08-28 16:57:30 +0300

    Project: Smart Calculator. Stage 2/7: 2+2+. Complete.

--- Smart Calculator/Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/Smart Calculator/task/calculator/calculator.py
@@ -1,1 +1,11 @@
-print(sum(int(x) for x in input().split()))
+def main():
+    answer = input().strip()
+    while answer != '/exit':
+        if answer:
+            print(sum(int(x) for x in answer.split()))
+        answer = input().strip()
+    print('Bye!')
+
+
+if __name__ == '__main__':
+    main()
