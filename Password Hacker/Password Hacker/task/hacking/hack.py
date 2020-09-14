bfb064d8b6715f1fe25c8d9342c1e72644abceb7
akocur <akocur@yandex.ru>
2020-09-14 20:07:45 +0300

    Project: Password Hacker. Stage 1/5: Establishing a connection. Add argparse

--- Password Hacker/Password Hacker/task/hacking/hack.py
+++ Password Hacker/Password Hacker/task/hacking/hack.py
@@ -2,8 +2,13 @@ import argparse
 
 
 def main():
-    pass
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('hostname', default='127.0.0.1', help='host name or IP address')
+    arg_parser.add_argument('port', type=int)
+    arg_parser.add_argument('message', help='message for sending to hostname')
+    args = arg_parser.parse_args()
+    print(args)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
