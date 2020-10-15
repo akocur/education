e94e0f5b20f7b67d77fe0c84c9c54f167e69c81b
akocur <akocur@yandex.ru>
2020-10-15 14:57:43 +0300

    Project:  Regex Engine. Stage 2/6: Matching two equal length strings. Done.

--- Regex Engine/Regex Engine/task/regex/regex.py
+++ Regex Engine/Regex Engine/task/regex/regex.py
@@ -1,10 +1,16 @@
+from itertools import zip_longest
+
 
 def main():
-    print(is_contains(*input().split('|')))
+    print(equals(*input().split('|')))
 
 
 def is_contains(regex, input_str):
     return not regex or regex == '.' or regex == input_str
+
+
+def equals(regex, str_):
+    return all(is_contains(r, s) for r, s in zip_longest(regex, str_, fillvalue=''))
 
 
 if __name__ == '__main__':
