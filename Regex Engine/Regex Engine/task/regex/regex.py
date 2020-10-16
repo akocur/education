2228d36035b3d91977364e823431187c1ea4139d
akocur <akocur@yandex.ru>
2020-10-16 16:43:30 +0300

    Project:  Regex Engine. Stage 3/6: Working with strings of different length. Done.

--- Regex Engine/Regex Engine/task/regex/regex.py
+++ Regex Engine/Regex Engine/task/regex/regex.py
@@ -2,15 +2,22 @@ from itertools import zip_longest
 
 
 def main():
-    print(equals(*input().split('|')))
+    print(is_matched(*input().split('|')))
+
+
+def is_matched(regex, str_):
+    for i in range(len(str_)):
+        if is_equal(regex, str_[i:]):
+            return True
+    return regex == ''
+
+
+def is_equal(regex, str_):
+    return all(is_contains(r, s) for r, s in zip_longest(regex, str_, fillvalue=''))
 
 
 def is_contains(regex, input_str):
     return not regex or regex == '.' or regex == input_str
-
-
-def equals(regex, str_):
-    return all(is_contains(r, s) for r, s in zip_longest(regex, str_, fillvalue=''))
 
 
 if __name__ == '__main__':
