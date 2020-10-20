c18313de8398aff99c54911f523c9b2fec2f7f82
akocur <akocur@yandex.ru>
2020-10-20 19:56:10 +0300

    Project:  Regex Engine. Stage 5/6: Controlling repetition. Done.

--- Regex Engine/Regex Engine/task/regex/regex.py
+++ Regex Engine/Regex Engine/task/regex/regex.py
@@ -1,7 +1,3 @@
-from distutils.command.register import register
-from itertools import zip_longest
-
-
 def main():
     print(is_matched(*input().split('|')))
 
@@ -19,51 +15,39 @@ def is_matched(regex, str_):
 
 
 def is_equal(regex, str_):
+
+    def get_range_for_repetition_operators(operator, string_):
+        if operator == '?':
+            return 0, 2
+        if operator == '*':
+            return 0, len(string_) + 1
+        if operator == '+':
+            return 1, len(string_) + 1
+
     equals = True
     i_str = 0
     prev_r = None
     for i_regex, r in enumerate(regex):
         s = str_[i_str] if i_str < len(str_) else ''
-
-        if r == '?':
+        if r in '?*+':
             residual_str = str_[i_str - 1:] if equals else str_[i_str:]
-            residual_regex_1 = regex[i_regex + 1:]
-            for i in range(2):
-                if is_equal((prev_r * i) + residual_regex_1, residual_str):
+            residual_regex = regex[i_regex + 1:]
+            for i in range(*get_range_for_repetition_operators(r, residual_str)):
+                if is_equal((prev_r * i) + residual_regex, residual_str):
                     return True
             return False
-
-        if r == '*':
-            residual_str = str_[i_str - 1:] if equals else str_[i_str:]
-            residual_regex_1 = regex[i_regex + 1:]
-            for i in range(len(residual_str)):
-                if is_equal((prev_r * i) + residual_regex_1, residual_str):
-                    return True
-            return False
-
-        if r == '+':
-            residual_str = str_[i_str - 1:] if equals else str_[i_str:]
-            residual_regex_1 = regex[i_regex + 1:]
-            for i in range(1, len(residual_str) + 1):
-                if is_equal((prev_r * i) + residual_regex_1, residual_str):
-                    return True
-            return False
-
         if prev_r != r and not equals:
             return False
         else:
-            if is_char_matched(r, s):
+            if are_characters_matched(r, s):
                 i_str += 1
             else:
                 equals = False
-
         prev_r = r
-
     return equals
-    # return all(is_contains(r, s) for r, s in zip_longest(regex, str_, fillvalue=''))
 
 
-def is_char_matched(regex, input_str):
+def are_characters_matched(regex, input_str):
     return not regex or regex == '.' or regex == input_str or (regex == '$' and not input_str)
 
 
